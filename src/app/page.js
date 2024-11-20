bea22e6ba56f78fab546e32159b1766bac087336
abhaypratapdeveloper <abhaypratapdeveloper@gmail.com>
2024-11-20 14:02:59 +0530

    Added card

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Banner from './Components/Banner'
-
+import Card from './Components/Card'
 import pages from '@/app/page.module.css'
 
 const page = () => {
   return (
     <>
-    <Banner Urlimage={"/Card6.jpg"} title={"Transform Your Spaces With Modern Designs."} />    
+    <Banner Urlimage={"/Card6.jpg"} title={"Transform Your Spaces With Modern Designs."} />
+
+    <div className={pages.cardflex}>
+      <Card image={"/Card1.jpg"} text={"Furnished 2 BHK"}/>
+      </div>    
     </>
   )
 }
