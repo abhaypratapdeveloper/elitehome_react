78ecd9435732d4c2ea3efc4928abac5537153c5c
abhaypratapdeveloper <abhaypratapdeveloper@gmail.com>
2024-12-15 12:16:22 +0530

    home page

---
+++ src/app/home/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../Components/Banner'
+
+const page = () => {
+  return (
+    <>
+    <div className=''>
+        <Banner/>
+    </div>
+    </>
+  )
+}
+
+export default page
